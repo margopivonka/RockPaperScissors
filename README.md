718c63fcac11ae98ec1425ffa1d779a1c60a0a06
margopivonka <59657652+margopivonka@users.noreply.github.com>
2020-03-23 17:59:58 -0400

    final commit?

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RockPaperScissors
+
+To set up game environment:
+    "conda create -n my-game-env python=3.7"
+    "conda activate my-game-env"
+
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,1 +1,55 @@
+from random import randint
+
+
+
+print("Welcome to...")
 print("Rock, Paper, Scissors, Shoot!")
+print("--------------------")
+
+#create list of possible variables
+options = ["Rock", "Paper", "Scissors"]
+
+#make cpu pick random vairable
+computer_choice = options[randint(0,2)]
+
+#set player to false
+player_choice = False
+
+while player_choice == False:
+    player_choice = input("Rock, Paper, Scissors?: ")
+    print("--------------------")
+    print("The computer chose: ", computer_choice)
+    print("--------------------")
+
+    if player_choice == computer_choice:
+            print("You Tie!")
+    elif player_choice == "Rock":
+        if computer_choice == "Paper":
+            print("Computer Wins!")
+        else:
+            print("You Win!")
+    elif player_choice == "Paper":
+        if computer_choice == "Rock":
+            print("You Win!")
+        else:
+            print("Computer Wins!")
+    elif player_choice == "Scissors":
+        if computer_choice == "Paper":
+            print("You Win!")
+        else:
+            print("Computer Wins!")
+    else:
+        print("That is not a valid option, please try again.")
+player_choice = False
+computer_choice = options[randint(0,2)]
+
+
+
+
+
+
+#user_input = input("Please select and option: Rock, Paper, or Scissors - ")
+#print(user_input)
+#
+#if user_input != "rock":
+
